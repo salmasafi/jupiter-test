e9c980d9757f452f812031372b3ac1095549e590
salmasafi <salmasafi26@gmail.com>
2024-07-29 21:20:09 +0300

    update1

--- lib/main.dart
+++ lib/main.dart
@@ -2,7 +2,7 @@ import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'features/login/presentation/views/login_screen.dart';
 import 'firebase_options.dart';
-
+//////heloooooooooooooo
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
